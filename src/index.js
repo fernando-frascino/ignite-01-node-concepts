bbee27e5f58cfb89476e23f74cc0b8acf67d77b0
Fernando Frascino <fernandofrascino@yahoo.com.br>
2021-06-10 08:29:00 -0300

    First commit

---
+++ src/index.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const cors = require('cors');
+
+const { v4: uuidv4 } = require('uuid');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const users = [];
+
+function checksExistsUserAccount(request, response, next) {
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+  if(!user) return response.status(404).json({ error: 'Username does not exists' });
+
+  request.user = user;
+
+  return next();
+}
+
+app.post('/users', (request, response) => {
+  const { name, username } = request.body;
+
+  const isUsernameUsed = users.some(user => user.username === username);
+  if(isUsernameUsed) return response.status(400).json({ error: 'Username has already been used'})
+
+  const user = {
+    id: uuidv4(),
+    name,
+    username,
+    todos: []
+  }
+
+  users.push(user);
+
+  return response.status(201).json(user);
+});
+
+app.get('/todos', checksExistsUserAccount, (request, response) => {
+  const { user } = request;
+
+  return response.json(user.todos);
+});
+
+app.post('/todos', checksExistsUserAccount, (request, response) => {
+  // Complete aqui
+});
+
+app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
+  // Complete aqui
+});
+
+app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
+  // Complete aqui
+});
+
+app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
+  // Complete aqui
+});
+
+module.exports = app;
